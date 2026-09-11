6cb117dd2af3a8e08cff186545860c5cf851c908
juliaselken22-code <juliaselken22@gmail.com>
2026-09-11 13:47:42 -0500

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,9 +1,15 @@
 document.getElementById("navigation").innerHTML = `
     <nav class="main-nav">
+
         <a href="index.html">Home</a>
+
         <a href="about.html">About</a>
+
         <a href="product.html">Product</a>
-        <a href="experience.html">Experience</a>
+
+        <a href="portfolio.html">Portfolio</a>
+
         <a href="contact.html">Contact</a>
+
     </nav>
 `;
